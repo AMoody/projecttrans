71f05b31047e5af0a5418f1fcb7003d46cc77d73
moodya71 <arthur.moody@bbc.co.uk>
2012-02-22 14:53:18 +0000

    Initital test

--- src/jprojecttranslator/Main.java
+++ src/jprojecttranslator/Main.java
@@ -1,6 +1,6 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * jProjectTranslator is used to translate audio editing projects from one format to another.
+ * Internally
  */
 
 package jprojecttranslator;
