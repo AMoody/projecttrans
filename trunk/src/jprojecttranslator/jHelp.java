5d63ab65c082693a738df1a1c12aa62760f1f8fe
moodya71 <moodya71@85ee3d4d-4800-4813-911c-63a28eb766e9>
2012-04-12 15:41:40 +0000

    Added 'version' dialogue to help menu.

--- trunk/src/jprojecttranslator/jHelp.java
+++ trunk/src/jprojecttranslator/jHelp.java
@@ -10,14 +10,33 @@ import java.util.Iterator;
  *
  * @author scobeam
  */
-public class jAbout extends javax.swing.JDialog {
+public class jHelp extends javax.swing.JDialog {
 
     /**
-     * Creates new form jAbout
+     * Creates new form jHelp
      */
-    public jAbout(java.awt.Frame parent, boolean modal) {
+    public jHelp(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
+        java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+        int intWindowWidth, intWindowHeight, intWindowX, intWindowY;
+        intWindowWidth = screenSize.width/2;
+        intWindowHeight = 7 * screenSize.height/8;
+        intWindowX = (screenSize.width-intWindowWidth)/2;
+        intWindowY = (screenSize.height-intWindowHeight)/2;
+        setSize(new java.awt.Dimension(intWindowWidth, intWindowHeight));
+        setLocation(intWindowX,intWindowY);
+        jEditorPane1.setContentType( "text/html" );
+        
+    }
+    
+    public void setText(String strText) {
+        jEditorPane1.setText(strText);
+    }
+    
+    public void loadAboutText() {
+        setName("About Project Translator");
+        setTitle("About Project Translator");
         String strReaders = "";
         Iterator itr = jProjectTranslator.listReaders.iterator();
         jProjectReader tempProjectReader;
@@ -32,7 +51,11 @@ public class jAbout extends javax.swing.JDialog {
                 + "<h1>Importers</h1><br>"
                 + "" + strReaders + "</html>");
     }
-
+    
+    public void setWindowTitle(String strTitle){
+        setTitle(strTitle);
+        setName(strTitle);
+    }
     /**
      * This method is called from within the constructor to initialise the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -133,13 +156,13 @@ public class jAbout extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(jAbout.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(jHelp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(jAbout.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(jHelp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(jAbout.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(jHelp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(jAbout.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(jHelp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -149,7 +172,7 @@ public class jAbout extends javax.swing.JDialog {
         java.awt.EventQueue.invokeLater(new Runnable() {
 
             public void run() {
-                jAbout dialog = new jAbout(new javax.swing.JFrame(), true);
+                jHelp dialog = new jHelp(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
 
                     @Override
