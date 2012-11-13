d9a61769d06ef6cd5831ea79e39dc767df5ddff8
scobeam <scobeam@85ee3d4d-4800-4813-911c-63a28eb766e9>
2012-11-13 09:20:30 +0000

    More work on Ardour writer. The exported ardour project will open with regions in the correct place on the playlist. I could not get some mixer settings (panning) to work but if they're left out of the project Ardour fills them in with sane defaults. Started work on region fades.

--- trunk/src/jprojecttranslator/fade.java
+++ trunk/src/jprojecttranslator/fade.java
@@ -1,11 +1,14 @@
 package jprojecttranslator;
 import org.dom4j.Element;
 import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import net.sourceforge.openforecast.DataSet;
 import net.sourceforge.openforecast.DataPoint;
 import net.sourceforge.openforecast.Observation;
 import net.sourceforge.openforecast.Forecaster;
 import net.sourceforge.openforecast.ForecastingModel;
+import org.dom4j.DocumentHelper;
 /**
  * This stores information about an audio fade.
  * A fade consists of a duration in samples and some curve data.
@@ -16,7 +19,10 @@ import net.sourceforge.openforecast.ForecastingModel;
 public class fade {
     // The fade length
     private long lLength; 
+    // An AES31 format string for the fade shape
     private String strFade;
+    // A hash map containing the fade as key value pairs. The key is the time in samples, the value is the gain between 1 and 0
+    private HashMap map;
     /**
      * Load a FadeIn or FadeOut from an Ardour project file.
      * Crossfades in Ardour consist of a FadeIn and FadeOut.
@@ -32,7 +38,7 @@ public class fade {
         strFade = "";
         String strKey, strValue;
         float fValue;
-        HashMap map = new HashMap();
+        map = new HashMap();
         Element xmlPoint;
         for (Iterator i = xmlFade.elementIterator("point");i.hasNext();) {
             xmlPoint = (Element)i.next();
@@ -124,7 +130,7 @@ public class fade {
         strFade = "";
         String strKey, strValue;
         float fValue;
-        HashMap map = new HashMap();
+        map = new HashMap();
         if ((xmlSource.attributeValue("active")!= null && xmlSource.attributeValue("active").equalsIgnoreCase("no") )
                 || (xmlSource.attributeValue("default")!= null &&xmlSource.attributeValue("default").equalsIgnoreCase("yes"))) {
             return false;
@@ -207,6 +213,51 @@ public class fade {
         return true;
         
     }
+    
+    public boolean loadAES31Fade (long lSetLength, String strFadeShape) {
+        lLength = lSetLength;
+        strFade = strFadeShape;
+        map = new HashMap();
+        String strFadeType = "";
+        float fValue0, fValue1, fValue2, fValue3, fValue4;
+        Pattern pPattern = Pattern.compile("(CURVE|LIN)\\s*"
+                + "([+-]?\\d+\\.?\\d*|_)\\s*"
+                + "([+-]?\\d+\\.?\\d*|_)\\s*"
+                + "([+-]?\\d+\\.?\\d*|_)");
+        Matcher mMatcher = pPattern.matcher(strFadeShape);
+        if (mMatcher.find()) {
+            strFadeType = mMatcher.group(1);
+            if (strFadeType.equalsIgnoreCase("CURVE")) {
+                fValue1 = Float.parseFloat(mMatcher.group(2));
+                fValue2 = Float.parseFloat(mMatcher.group(3));
+                fValue3 = Float.parseFloat(mMatcher.group(4));
+                fValue1 = (float)Math.exp(fValue1/20);
+                fValue2 = (float)Math.exp(fValue2/20);
+                fValue3 = (float)Math.exp(fValue3/20);
+                if (fValue1 > fValue3) {
+                    fValue0 = 1;
+                    fValue4 = 0;
+                } else {
+                    fValue0 = 0;
+                    fValue4 = 1;
+                }
+                // The map consists of keys which are the time in samples across the fade, and values which are a float between 1 and 0.
+                map.put(0, fValue0);
+                map.put(lSetLength/4, fValue1);
+                map.put(lSetLength/2, fValue2);
+                map.put(3*lSetLength/4, fValue3);
+                map.put(lSetLength, fValue4);
+            } else {
+                return false;
+            }
+            
+            
+            
+        } else {
+            return false;
+        }
+        return true;
+    }
     /**
      * @return Returns the length of the fade in samples.
      */
@@ -218,5 +269,32 @@ public class fade {
      */
     public String getFade() {
         return strFade;
+    }
+    
+    public Element getArdourFade(int setID) {
+        Element xmlFade;
+        if (Float.parseFloat(map.get(0).toString()) == 1) {
+            // It's a fade out
+            xmlFade = DocumentHelper.createElement("FadeOut").addAttribute("active","yes");
+        } else {
+            // It's a fade in
+            xmlFade = DocumentHelper.createElement("FadeIn").addAttribute("active","yes");
+        }
+        // <AutomationList id="108" default="1" min_yval="0" max_yval="2" max_xval="0" state="Off" style="Absolute">
+        Element xmlEvents = xmlFade.addElement("AutomationList").addAttribute("id", "" + setID).addAttribute("default", "1").addAttribute("min_yval", "0")
+                .addAttribute("max_yval", "2").addAttribute("max_xval", "0").addAttribute("state", "Off")
+                .addAttribute("style", "Absolute").addElement("events");
+        String strEvents = "";
+        Set s = map.entrySet();
+        Iterator i = s.iterator();
+        Map.Entry me;
+        while (i.hasNext()) {
+            me = (Map.Entry)i.next();
+            strEvents = strEvents + Float.parseFloat("" + me.getKey()) + " " + Float.parseFloat("" + me.getValue()) + "\n";
+           
+        }
+        xmlEvents.addText(strEvents);
+         
+        return xmlFade;
     }
 }
